deb99462d482fc86d3c603365dfcb51a5ab32934
abdo160996 <abdo160996@gmail.com>
2023-05-10 14:43:15 +0300

    update intro page

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,23 +3,34 @@ import BarLoader from "react-spinners/BarLoader";
 import useFetch from "./useFetch";
 
 const Intro = (props) => {
-   const { data : categories,isLoading }= useFetch('https://opentdb.com/api_category.php')
-   
+  const { data: categories, isLoading } = useFetch("https://opentdb.com/api_category.php");
+
   return (
     <div className="container">
-      
       <div className="intro-wrapper position-absolute top-50 start-50 translate-middle d-flex align-items-center justify-content-center">
         <div className="info d-flex flex-column align-items-center">
           <h1 className="">Quizzical App</h1>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
-          {props.isLoading ?
-           <BarLoader color="#293264" loading={isLoading} size={25} aria-label="Loading Spinner" data-testid="loader" /> 
-           :<Button className="rounded-pill w-50" variant="primary" onClick={props.handleClick}>
-            Start Quiz
-            </Button>}
-          <select className="mt-3  form-select" value={props.category} onChange={(e)=>{props.setCategory(e.target.value)}}>
-            {categories?.trivia_categories.map(cate=> <option value={cate.id}>{cate.name}</option>)}
-          </select>
+          {props.isLoading ? (
+            <BarLoader color="#293264" loading={isLoading} size={25} aria-label="Loading Spinner" data-testid="loader" />
+          ) : (
+            <>
+              <Button className="rounded-pill w-50" variant="primary" onClick={props.handleClick}>
+                Start Quiz
+              </Button>
+              <select
+                className="mt-3  form-select"
+                value={props.category}
+                onChange={(e) => {
+                  props.setCategory(e.target.value);
+                }}
+              >
+                {categories?.trivia_categories.map((cate) => (
+                  <option value={cate.id}>{cate.name}</option>
+                ))}
+              </select>
+            </>
+          )}
         </div>
       </div>
     </div>
